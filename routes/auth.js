fa9ad90a61d571fa8e104f9c075f0c25789bc291
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2022-03-10 13:01:24 +0000

    adding controller for register and login

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,20 @@
-// its express
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const{ register, login, forgotpassword, resetpassword } = require('../controllers/auth');
+// Controllers
+const {
+  register,
+  login,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/auth");
 
 router.route("/register").post(register);
 
 router.route("/login").post(login);
 
-router.route("/forgotpassword").post(forgotpassword);
+router.route("/forgotpassword").post(forgotPassword);
 
-router.route("/resetpassword/:resetToken").put(resetpassword)
+router.route("/passwordreset/:resetToken").put(resetPassword);
+
 module.exports = router;
