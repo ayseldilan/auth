78ad642006e824458b2677773a97e32214abc945
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2022-03-10 17:33:21 +0000

    adding email section

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
+const crypto = require("crypto");
 const User = require('../models/User');
 const ErrorResponse = require('../utils/errorResponse');
+const sendEmail = require("../utils/sendEmail");
 
 exports.register = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -46,26 +48,86 @@ exports.login = async (req, res, next) => {
   }
 };
 
-exports.forgotpassword = async (req, res, next) => {
-    const {email} = req.body;
+exports.forgotPassword = async (req, res, next) => {
+  // Send Email to email provided but first check if user exists
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return next(new ErrorResponse("No email could not be sent", 404));
+    }
+
+    // Reset Token Gen and add to database hashed (private) version of token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save();
+
+    // Create reset url to email to provided email
+    const resetUrl = `http://localhost:3000/passwordreset/${resetToken}`;
+
+    // HTML Message
+    const message = `
+      <h1>You have requested a password reset</h1>
+      <p>Please make a put request to the following link:</p>
+      <a href=${resetUrl} clicktracking=off>${resetUrl}</a>
+    `;
 
     try {
-      const user = await User.findOne({email});
+      await sendEmail({
+        to: user.email,
+        subject: "Password Reset Request",
+        text: message,
+      });
 
-      if(!user) {
-        return next(new ErrorResponse("Email could not be sent", 404))
-      }
+      res.status(200).json({ success: true, data: "Email Sent" });
+    } catch (err) {
+      console.log(err);
 
-      const resetToken = user.getResetPasswordToken();
+      user.resetPasswordToken = undefined;
+      user.resetPasswordExpire = undefined;
 
       await user.save();
-    } catch (error) {
-      
+
+      return next(new ErrorResponse("Email could not be sent", 500));
     }
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.resetpassword = async (req, res, next) => {
-    res.send("Reset Password Route");
+exports.resetPassword = async (req, res, next) => {
+  // Compare token in URL params to hashed token
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.resetToken)
+    .digest("hex");
+
+  try {
+    const user = await User.findOne({
+      resetPasswordToken,
+      resetPasswordExpire: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return next(new ErrorResponse("Invalid Token", 400));
+    }
+
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    res.status(201).json({
+      success: true,
+      data: "Password Updated Success",
+      token: user.getSignedJwtToken(),
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 const sendToken = (user, statusCode, res) => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
-const crypto = require('crypto');
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
@@ -25,42 +25,43 @@ const UserSchema = new mongoose.Schema({
     },
     resetPasswordToken: String,
     resetPasswordExpire: Date,
-});
-
-UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    next();
-  }
-
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-UserSchema.methods.matchPassword = async function (password) {
-  return await bcrypt.compare(password, this.password);
-};
-
-UserSchema.methods.getSignedJwtToken = function () {
-  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
   });
-};
-
-UserSchema.methods.getResetPasswordToken = function() {
-  const resetToken = crypto.randomBytes(20), toString("hex");
-
-  this.resetPasswordToken = crypto
-  .createHash("sha256")
-  .update(resetToken)
-  .digest("hex");
-
-  this.resetPasswordExpire = Date.now() + 10 * (60 * 1000)
-
-  return resetToken;
-
-}
-
-const User = mongoose.model("User", UserSchema);
-
-module.exports = User;
+  
+  UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+      next();
+    }
+  
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  });
+  
+  UserSchema.methods.matchPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  };
+  
+  UserSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRE,
+    });
+  };
+  
+  UserSchema.methods.getResetPasswordToken = function () {
+    const resetToken = crypto.randomBytes(20).toString("hex");
+  
+    // Hash token (private key) and save to database
+    this.resetPasswordToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
+  
+    // Set token expire date
+    this.resetPasswordExpire = Date.now() + 10 * (60 * 1000); // Ten Minutes
+  
+    return resetToken;
+  };
+  
+  const User = mongoose.model("User", UserSchema);
+  
+  module.exports = User;
