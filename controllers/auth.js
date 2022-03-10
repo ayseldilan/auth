4160aebac2392ceb2e33810f58c46cd6c6b90f1e
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2022-03-10 13:44:40 +0000

    handling errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const ErrorResponse = require('../utils/errorResponse');
 
 exports.register = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -16,15 +17,39 @@ exports.register = async (req, res, next) => {
         });
 
     } catch (error) {
-        res.status(500).json({
-            success: false,
-            error: error.message,
-        });
+        next(error);
     }
 };
 
-exports.login = (req, res, next) => {
-    const
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        return next(new ErrorResponse("Please provide an email and password", 400))
+    }
+
+    try {
+        const user = await User.findOne({ email }).select("+password")
+
+        if(!user) {
+            return next(new ErrorResponse("Invalid Credentials", 401))
+        }
+
+        const isMatch = await user.matchPasswords(password);
+
+        if(!isMatch) {
+            return next(new ErrorResponse("Invalid Credentials", 401))
+        }
+
+        res.status(200).json({
+            success: true,
+            token: "tr2222",
+        });
+
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message});
+
+    }
 };
 
 exports.forgotpassword = (req, res, next) => {
