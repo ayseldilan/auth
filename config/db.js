22ed316ee9a8335a47c477e8f2b373c6dc92a82f
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2022-03-10 16:38:08 +0000

    adding authorization to the middleware

--- config/db.js
+++ config/db.js
@@ -11,4 +11,4 @@ const connectDB = async() => {
     console.log("MongoDB connected");
 };
 
-module.exports = connectDB;
+module.exports = connectDB
--- controllers/auth.js
+++ controllers/auth.js
@@ -2,55 +2,49 @@ const User = require('../models/User');
 const ErrorResponse = require('../utils/errorResponse');
 
 exports.register = async (req, res, next) => {
-    const {username, email, password} = req.body;
-
+    const { username, email, password } = req.body;
+  
     try {
-        const user = await User.create({
-            username, 
-            email, 
-            password,
-        });
-
-        res.status(201).json({
-            success: true,
-            user,
-        });
-
-    } catch (error) {
-        next(error);
+      const user = await User.create({
+        username,
+        email,
+        password,
+      });
+  
+      sendToken(user, 200, res);
+    } catch (err) {
+      next(err);
     }
 };
 
 exports.login = async (req, res, next) => {
     const { email, password } = req.body;
-
-    if(!email || !password) {
-        return next(new ErrorResponse("Please provide an email and password", 400))
+  
+    // Check if email and password is provided
+    if (!email || !password) {
+      return next(new ErrorResponse("Please provide an email and password", 400));
     }
-
+  
     try {
-        const user = await User.findOne({ email }).select("+password")
-
-        if(!user) {
-            return next(new ErrorResponse("Invalid Credentials", 401))
-        }
-
-        const isMatch = await user.matchPasswords(password);
-
-        if(!isMatch) {
-            return next(new ErrorResponse("Invalid Credentials", 401))
-        }
-
-        res.status(200).json({
-            success: true,
-            token: "tr2222",
-        });
-
-    } catch (error) {
-        res.status(500).json({ success: false, error: error.message});
-
+      // Check that user exists by email
+      const user = await User.findOne({ email }).select("+password");
+  
+      if (!user) {
+        return next(new ErrorResponse("Invalid credentials", 401));
+      }
+  
+      // Check that password match
+      const isMatch = await user.matchPassword(password);
+  
+      if (!isMatch) {
+        return next(new ErrorResponse("Invalid credentials", 401));
+      }
+  
+      sendToken(user, 200, res);
+    } catch (err) {
+      next(err);
     }
-};
+  };
 
 exports.forgotpassword = (req, res, next) => {
     res.send("Forgot Password Route");
@@ -58,4 +52,9 @@ exports.forgotpassword = (req, res, next) => {
 
 exports.resetpassword = (req, res, next) => {
     res.send("Reset Password Route");
+};
+
+const sendToken = (user, statusCode, res) => {
+    const token = user.getSignedJwtToken();
+    res.status(statusCode).json({ sucess: true, token });
 };
