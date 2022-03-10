aa8c5a0ee9d01c2ef78bf35dc1117e7df01e4032
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2022-03-10 18:38:27 +0000

    adding routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
   return <div className="App"> React App </div>;
 
-}
+  <Router>
+    <div className='app'>
+      <Switch>
+        <Route exact path="/login" component={LoginScreen/>
+        <Route exact path="/register" component={RegisterScreen/>
+        <Route exact path="/forgotpassword" component={ForgotPasswordScreen/>
+        <Route exact path="/passwordreset/:resetToken" component={PasswordResetScreen/>
+      </Switch>
+      </div>
+  </Router>
+
+};
 
 export default App;
---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,20 @@
+import {Redirect, Route} from 'react-router-dom';
+
+function PrivateRoute({component: Component, ...rest}) {
+  return (
+    <Route
+    {...rest}
+    render={
+        (props) => {
+            localStorage.getItem("authToke") ? (
+                <Component {...props} />
+            ) : (
+                <Redirect to="/login" />
+            )
+        }
+    }
+    >
+  )
+}
+
+export default PrivateRoute
